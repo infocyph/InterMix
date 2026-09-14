b70c09e77efd16a5dfef939c882aa82005009ac2
A. B. M. Mahmudul Hasan <abmmhasan@users.noreply.github.com>
2026-09-14 23:14:39 +0600

    style(di): align compiled scope state ordering

--- src/DI/Internal/ScopeState.php
+++ src/DI/Internal/ScopeState.php
@@ -7,14 +7,14 @@ namespace Infocyph\InterMix\DI\Internal;
 /** @internal */
 final class ScopeState
 {
+    public readonly bool $hasSeeds;
+
     public int $attachments = 0;
 
     public bool $closed = false;
 
     /** @var array<int, string> service slot => constructing carrier */
     public array $constructing = [];
-
-    public readonly bool $hasSeeds;
 
     /** @var array<int, mixed> */
     public array $resolved = [];
